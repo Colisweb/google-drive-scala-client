5883204fb62bc048823aeb57fea5496fed3d0a96
Michel Daviot <michel@daviot.info>
2020-04-16 18:06:37 +0200

    chore: sample with read/write in spreadsheet

--- src/test/scala/Demo.scala
+++ src/test/scala/Demo.scala
@@ -3,14 +3,16 @@ import com.colisweb.gdrive.client._
 import scala.collection.JavaConverters._
 
 object Demo extends App {
-  val sheetId = "1a0zzSgpder5tzfvc6HZAF14g97fOenz00solfZI7vm4"
+  val sheetId = "1R_rshsVSQkfehwP-4R_Fb2f5Ilgwjeu-nzxvStSPRg8"
 
   val client = GoogleClient("google-credentials.json", "RoutingAnalysis")
   val rows = GoogleSheetsClient(client.sheetsService)
-    .readRows("A1:AL5", sheetId)
+    .readRows("A1:C4", sheetId)
     .map(_.getValues.asScala)
     .map(_.map(_.getFormattedValue))
 
   rows.foreach(println)
+
+  GoogleSheetsClient(client.sheetsService).writeRange(sheetId, "A1:D3", rows.transpose)
 
 }
