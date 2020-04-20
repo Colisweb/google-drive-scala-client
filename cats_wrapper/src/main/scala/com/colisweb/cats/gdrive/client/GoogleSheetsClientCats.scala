217a08b2c7f4d9724f0fca6cc7554567bd3cc35c
arthur <arthur.teisseire@epitech.eu>
2020-04-20 16:22:43 +0200

    Add GoogleSheetsClientCats

---
+++ cats_wrapper/src/main/scala/com/colisweb/cats/gdrive/client/GoogleSheetsClientCats.scala
@@ -0,0 +1,14 @@
+package com.colisweb.cats.gdrive.client
+
+import cats.effect.Sync
+import com.colisweb.gdrive.client.{GoogleAuthenticator, GoogleSheetsClient}
+
+class GoogleSheetsClientCats[F[_]](authenticator: GoogleAuthenticator)(implicit F: Sync[F]) {
+
+  private val client = new GoogleSheetsClient(authenticator)
+
+  def createWithSheets(spreadSheetTitle: String, titles: List[String]): F[client.GoogleSpreadsheet] =
+    F.delay(
+      client.createWithSheets(spreadSheetTitle, titles)
+    )
+}
