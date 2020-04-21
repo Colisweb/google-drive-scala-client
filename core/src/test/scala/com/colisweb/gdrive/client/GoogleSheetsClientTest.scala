805bebb6facafdb9a39604ab7f57aaaf0d55e520
arthur <arthur.teisseire@epitech.eu>
2020-04-21 09:50:46 +0200

    Test write on invalid sheet name

--- core/src/test/scala/com/colisweb/gdrive/client/GoogleSheetsClientTest.scala
+++ core/src/test/scala/com/colisweb/gdrive/client/GoogleSheetsClientTest.scala
@@ -1,5 +1,6 @@
 package com.colisweb.gdrive.client
 
+import com.google.api.client.googleapis.json.GoogleJsonResponseException
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -7,7 +8,7 @@ import scala.collection.JavaConverters._
 
 class GoogleSheetsClientTest extends AnyFlatSpec with Matchers {
 
-  it should "write to and read from a google spreadsheet" in {
+  it should "test write to and read from a google spreadsheet" in {
 
     val authenticator = GoogleAuthenticator("google-credentials.json", "RoutingAnalysis")
     val drive         = new GoogleDriveClient(authenticator)
@@ -29,6 +30,20 @@ class GoogleSheetsClientTest extends AnyFlatSpec with Matchers {
       .map(_.getValues.asScala.map(_.getFormattedValue))
 
     rowData shouldBe data
+
+    drive.delete(spreadSheet.id)
+  }
+
+  it should "test write in an invalid sheet name" in {
+    val authenticator = GoogleAuthenticator("google-credentials.json", "RoutingAnalysis")
+    val drive         = new GoogleDriveClient(authenticator)
+
+    val sheetNames  = List("foo", "toto")
+    val spreadSheet = GoogleSpreadsheet.createWithSheets(authenticator, "spreadsheet_name", sheetNames)
+
+    val data = Seq.tabulate(2, 3)((r, c) => s"data $r $c")
+
+    a [GoogleJsonResponseException] should be thrownBy spreadSheet.writeRange("invalid_sheet_name!A1:C2", data)
 
     drive.delete(spreadSheet.id)
   }
