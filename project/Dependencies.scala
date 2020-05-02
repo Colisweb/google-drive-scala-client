91479ca28ed7475746a5292521bc7a113ed98519
Scala Steward <gitlab@colisweb.com>
2020-05-02 01:14:01 +0000

    Update google-api-services-drive to v3-rev20200413-1.30.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object GoogleDrive {
   final val client = "com.google.api-client"   % "google-api-client"          % "1.30.9"
   final val oauth  = "com.google.oauth-client" % "google-oauth-client"        % "1.30.6"
   final val sheets = "com.google.apis"         % "google-api-services-sheets" % "v4-rev20200312-1.30.9"
-  final val drive  = "com.google.apis"         % "google-api-services-drive"  % "v3-rev20200326-1.30.9"
+  final val drive  = "com.google.apis"         % "google-api-services-drive"  % "v3-rev20200413-1.30.9"
 
   final val all = Seq(client, sheets, oauth, drive)
 }
