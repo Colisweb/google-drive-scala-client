e56ec532fe5447d89162c8f54e97f265cc213865
Scala Steward <gitlab@colisweb.com>
2020-05-15 01:13:41 +0000

    Update google-api-services-sheets to v4-rev20200508-1.30.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object GoogleDrive {
   final val client = "com.google.api-client"   % "google-api-client"          % "1.30.9"
   final val oauth  = "com.google.oauth-client" % "google-oauth-client"        % "1.30.6"
-  final val sheets = "com.google.apis"         % "google-api-services-sheets" % "v4-rev20200424-1.30.9"
+  final val sheets = "com.google.apis"         % "google-api-services-sheets" % "v4-rev20200508-1.30.9"
   final val drive  = "com.google.apis"         % "google-api-services-drive"  % "v3-rev20200413-1.30.9"
 
   final val all = Seq(client, sheets, oauth, drive)
