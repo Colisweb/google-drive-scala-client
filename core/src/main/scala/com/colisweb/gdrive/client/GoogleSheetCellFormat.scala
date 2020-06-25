c71caee925fc0bf60429b51712db5cb4b7d3cdfe
Simon Bar <simon.b@colisweb.com>
2020-06-25 11:00:00 +0200

    fix: method to combine cell formats

--- core/src/main/scala/com/colisweb/gdrive/client/GoogleSheetCellFormat.scala
+++ core/src/main/scala/com/colisweb/gdrive/client/GoogleSheetCellFormat.scala
@@ -8,6 +8,16 @@ trait GoogleSheetCellFormat {
   def format: CellFormat = combine(new CellFormat)
 }
 
+object GoogleSheetCellFormat {
+
+  def combineMultipleFormats(formats: List[GoogleSheetCellFormat]): GoogleSheetCellFormat =
+    new GoogleSheetCellFormat {
+      val combine: CellFormat => CellFormat =
+        format => formats.foldLeft(format) { case (acc, next) => next.combine(acc) }
+    }
+
+}
+
 case object BoldFormat extends GoogleSheetCellFormat {
   val combine: CellFormat => CellFormat = _.setTextFormat(new TextFormat().setBold(true))
 }
@@ -18,4 +28,12 @@ case object PercentageFormat extends GoogleSheetCellFormat {
 
 final case class BackgroundColorFormat(red: Float, green: Float, blue: Float) extends GoogleSheetCellFormat {
   val combine: CellFormat => CellFormat = _.setBackgroundColor(new Color().setRed(red).setGreen(green).setBlue(blue))
+}
+
+final case class HorizontalAlignmentFormat(alignment: String) extends GoogleSheetCellFormat {
+  val combine: CellFormat => CellFormat = _.setHorizontalAlignment(alignment)
+}
+
+final case class CellTextFormat(textFormat: TextFormat) extends GoogleSheetCellFormat {
+  val combine: CellFormat => CellFormat = _.setTextFormat(textFormat)
 }
