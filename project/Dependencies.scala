3c83aec816261b28bc3869f0942ac5907563e648
Scala Steward <gitlab@colisweb.com>
2020-07-09 01:14:40 +0000

    Update google-api-client to 1.30.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,10 +8,10 @@ object Versions {
   final val catsEffect        = "2.1.3"
   final val catsRetry         = "1.1.1"
   final val scalaCompat       = "2.1.6"
-  final val googleClient      = "1.30.9"
+  final val googleClient      = "1.30.10"
   final val googleAuth        = "0.21.1"
-  final val googleSheets      = "v4-rev20200616-1.30.9"
-  final val googleDrive       = "v3-rev20200609-1.30.9"
+  final val googleSheets      = "v4-rev20200616-1.30.10"
+  final val googleDrive       = "v3-rev20200609-1.30.10"
 }
 
 object Dependencies {
