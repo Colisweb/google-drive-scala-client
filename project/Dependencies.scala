09f3e7efeb443e6cf9fef60d03063bfca1477474
Scala Steward <gitlab@colisweb.com>
2020-07-23 01:13:29 +0000

    Update google-api-services-drive to v3-rev20200618-1.30.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Versions {
   final val googleClient      = "1.30.9"
   final val googleAuth        = "0.22.0"
   final val googleSheets      = "v4-rev20200616-1.30.9"
-  final val googleDrive       = "v3-rev20200618-1.30.9"
+  final val googleDrive       = "v3-rev20200618-1.30.10"
 }
 
 object Dependencies {
