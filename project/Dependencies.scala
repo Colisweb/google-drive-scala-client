2428dcf5c9e0eb8b224948b937846ed2363919d4
Scala Steward <gitlab@colisweb.com>
2021-03-22 02:14:26 +0000

    Update google-api-services-sheets to v4-rev20210309-1.31.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Versions {
   final val scalaCompat       = "2.4.2"
   final val googleClient      = "1.31.3"
   final val googleAuth        = "0.25.2"
-  final val googleSheets      = "v4-rev20210223-1.31.0"
+  final val googleSheets      = "v4-rev20210309-1.31.0"
   final val googleDrive       = "v3-rev20210228-1.31.0"
 }
 
