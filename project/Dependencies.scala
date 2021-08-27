bb61a5b3ccadb402fbc213423e90b02fa1c702d0
Scala Steward <gitlab@colisweb.com>
2021-08-27 22:09:58 +0000

    Update zio to 1.0.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Versions {
   final val googleAuth   = "1.1.0"
   final val googleSheets = "v4-rev20210629-1.32.1"
   final val googleDrive  = "v3-rev20210725-1.32.1"
-  final val zio          = "1.0.9"
+  final val zio          = "1.0.11"
 }
 
 object Dependencies {
