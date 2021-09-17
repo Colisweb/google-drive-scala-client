6bedaedde9728a945bcaf12007d43e169a6fb655
Scala Steward <gitlab@colisweb.com>
2021-09-17 22:10:08 +0000

    Update scalatest to 3.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object TestDependencies {
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9" % Test
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10" % Test
 }
 
 object Versions {
