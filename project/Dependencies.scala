a10e8389e5b72ba4bd9ea607855b89d0952d7b26
Scala Steward <gitlab@colisweb.com>
2021-11-12 23:09:20 +0000

    Update scala-collection-compat to 2.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object TestDependencies {
 object Versions {
   final val catsEffect   = "2.5.4"
   final val catsRetry    = "2.1.1"
-  final val scalaCompat  = "2.5.0"
+  final val scalaCompat  = "2.6.0"
   final val googleClient = "1.32.2"
   final val googleAuth   = "1.2.2"
   final val googleSheets = "v4-rev20210629-1.32.1"
