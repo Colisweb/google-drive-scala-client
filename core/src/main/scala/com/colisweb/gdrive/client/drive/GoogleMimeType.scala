c9cf694bbfd46641b934bf304bcf89abacc9afd2
Simon Bar <simon.b@colisweb.com>
2021-11-18 15:22:11 +0000

    fix: Add PNG mime type

--- core/src/main/scala/com/colisweb/gdrive/client/drive/GoogleMimeType.scala
+++ core/src/main/scala/com/colisweb/gdrive/client/drive/GoogleMimeType.scala
@@ -4,16 +4,19 @@ sealed trait GoogleMimeType
 case object GoogleSpreadsheetType extends GoogleMimeType
 case object GoogleDriveFolderType extends GoogleMimeType
 case object CsvFileType           extends GoogleMimeType
+case object PngImageType          extends GoogleMimeType
 
 object GoogleMimeType {
   val driveFolder = "application/vnd.google-apps.folder"
   val spreadsheet = "application/vnd.google-apps.spreadsheet"
   val csvFile     = "text/csv"
+  val png         = "image/png"
 
   def name(googleMimeType: GoogleMimeType): String =
     googleMimeType match {
       case GoogleDriveFolderType => driveFolder
       case GoogleSpreadsheetType => spreadsheet
       case CsvFileType           => csvFile
+      case PngImageType          => png
     }
 }
