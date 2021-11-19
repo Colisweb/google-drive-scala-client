716cc8b9dc04f8abc2db753b87d78581bb96eeac
Scala Steward <gitlab@colisweb.com>
2021-11-19 23:08:14 +0000

    Update google-cloud-bigquery to 2.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Versions {
   final val googleAuth     = "1.3.0"
   final val googleSheets   = "v4-rev20210629-1.32.1"
   final val googleDrive    = "v3-rev20211107-1.32.1"
-  final val googleBigQuery = "2.3.3"
+  final val googleBigQuery = "2.4.1"
   final val zio            = "1.0.12"
 }
 
