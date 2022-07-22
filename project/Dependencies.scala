8b32b054bf6ec233ab62eb8c2c287f1379eba858
Scala Steward <gitlab@colisweb.com>
2022-07-22 22:08:23 +0000

    Revert commit(s) aa34372

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Versions {
   final val googleClient   = "1.33.2"
   final val googleAuth     = "1.6.0"
   final val googleSheets   = "v4-rev20220308-1.32.1"
-  final val googleDrive    = "v3-rev20220709-1.32.1"
+  final val googleDrive    = "v3-rev20220214-1.32.1"
   final val googleBigQuery = "2.10.2"
   final val zio            = "1.0.13"
 }
