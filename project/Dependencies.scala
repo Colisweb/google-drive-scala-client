68cf52c2f95d41a48fbc4a499aaa9b5e665921ca
Scala Steward <gitlab@colisweb.com>
2022-07-22 22:08:11 +0000

    Update google-api-client to 1.35.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Versions {
   final val circe          = "0.14.2"
   final val scalaCompat    = "2.8.0"
   final val scalaReflect   = "2.13.8"
-  final val googleClient   = "1.33.4"
+  final val googleClient   = "1.35.2"
   final val googleAuth     = "1.7.0"
   final val googleSheets   = "v4-rev20220322-1.32.1"
   final val googleDrive    = "v3-rev20220417-1.32.1"
