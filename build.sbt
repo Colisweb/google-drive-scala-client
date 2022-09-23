7f9e96b597b948fe72a1e2d0072926d9ce12429a
Scala Steward <gitlab@colisweb.com>
2022-09-23 22:09:21 +0000

    Update scala-library, scala-reflect to 2.13.9

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 import CompileFlags._
 
 lazy val scala212               = "2.12.17"
-lazy val scala213               = "2.13.8"
+lazy val scala213               = "2.13.9"
 lazy val supportedScalaVersions = List(scala213, scala212)
 
 ThisBuild / organization       := "com.colisweb"
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   final val catsRetry      = "2.1.1"
   final val circe          = "0.14.3"
   final val scalaCompat    = "2.8.1"
-  final val scalaReflect   = "2.13.8"
+  final val scalaReflect   = "2.13.9"
   final val googleClient   = "2.0.0"
   final val googleAuth     = "1.11.0"
   final val googleSheets   = "v4-rev20220620-2.0.0"
