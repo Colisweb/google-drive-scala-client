3c0d2566a258f989502e2465eb6274af2ce9809e
Scala Steward <gitlab@colisweb.com>
2022-10-14 22:09:28 +0000

    Update scala-library, scala-reflect to 2.13.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   final val catsRetry      = "2.1.1"
   final val circe          = "0.14.3"
   final val scalaCompat    = "2.8.1"
-  final val scalaReflect   = "2.13.9"
+  final val scalaReflect   = "2.13.10"
   final val googleClient   = "2.0.0"
   final val googleAuth     = "1.11.0"
   final val googleSheets   = "v4-rev20220927-2.0.0"
