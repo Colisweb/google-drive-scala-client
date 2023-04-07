a1e232b58d207fe67a907caa39203e7fab70e94c
Scala Steward <gitlab@colisweb.com>
2023-04-07 22:06:49 +0000

    Update google-auth-library-credentials, ... to 1.16.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Versions {
   final val scalaCompat    = "2.9.0"
   final val scalaReflect   = "2.13.10"
   final val googleClient   = "2.2.0"
-  final val googleAuth     = "1.16.0"
+  final val googleAuth     = "1.16.1"
   final val googleSheets   = "v4-rev20230227-2.0.0"
   final val googleDrive    = "v3-rev20230306-2.0.0"
   final val googleBigQuery = "2.24.4"
