ed26c6db509513236f59aab8f3be979e46089a7f
colas.mombrun <colas.mombrun@colisweb.com>
2023-04-14 16:51:57 +0200

    fix: add excelSheet format

--- core/src/main/scala/com/colisweb/gdrive/client/drive/GoogleMimeType.scala
+++ core/src/main/scala/com/colisweb/gdrive/client/drive/GoogleMimeType.scala
@@ -2,20 +2,23 @@ package com.colisweb.gdrive.client.drive
 
 sealed trait GoogleMimeType
 case object GoogleSpreadsheetType extends GoogleMimeType
+case object ExcelSpreadsheetType  extends GoogleMimeType
 case object GoogleDriveFolderType extends GoogleMimeType
 case object CsvFileType           extends GoogleMimeType
 case object PngImageType          extends GoogleMimeType
 
 object GoogleMimeType {
   val driveFolder = "application/vnd.google-apps.folder"
-  val spreadsheet = "application/vnd.google-apps.spreadsheet"
+  val googleSheet = "application/vnd.google-apps.spreadsheet"
+  val excelSheet  = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
   val csvFile     = "text/csv"
   val png         = "image/png"
 
   def name(googleMimeType: GoogleMimeType): String =
     googleMimeType match {
       case GoogleDriveFolderType => driveFolder
-      case GoogleSpreadsheetType => spreadsheet
+      case GoogleSpreadsheetType => googleSheet
+      case ExcelSpreadsheetType  => excelSheet
       case CsvFileType           => csvFile
       case PngImageType          => png
     }
