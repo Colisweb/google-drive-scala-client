df14fcd1dc64df6c77f8a9295586b23e043aadf0
Scala Steward <gitlab@colisweb.com>
2023-06-09 22:07:29 +0000

    Update zio to 2.0.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Versions {
   final val googleSheets   = "v4-rev20230526-2.0.0"
   final val googleDrive    = "v3-rev20230610-2.0.0"
   final val googleBigQuery = "2.29.0"
-  final val zio            = "1.0.18"
+  final val zio            = "2.0.15"
 }
 
 object Dependencies {
