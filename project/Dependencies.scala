71c755ec63923d4f0005e914866847ae5e13b094
Scala Steward <gitlab@colisweb.com>
2023-08-25 22:06:16 +0000

    Update circe-generic-extras to 0.14.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object TestDependencies {
 object Versions {
   final val catsEffect     = "2.5.5"
   final val catsRetry      = "2.1.1"
-  final val circe          = "0.14.1"
+  final val circe          = "0.14.3"
   final val scalaCompat    = "2.11.0"
   final val scalaReflect   = "2.13.11"
   final val googleClient   = "2.2.0"
